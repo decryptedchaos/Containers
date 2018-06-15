98074a8cd31d54c5ba4f5bd15e642fd67b181edb
Joshua Kerr <nixgod@gmail.com>
2018-06-14 20:21:30 -0500

    move commands above export

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 sleep 2
 
-cd /home/container
+# Change to binary directory
 
-# Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+cd /home/container/ShooterGame/Binaries/Linux
 
 # Set Ulimit
 ulimit -n 500000
 echo "Set: ulimit -n 500000"
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
