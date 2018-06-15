10d0ef32566a2742a948c49dd2b47811c9d7997e
Joshua Kerr <nixgod@gmail.com>
2018-06-14 20:04:46 -0500

    change file open descriptors to 500000

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Set Ulimit
-ulimit -c unlimited
-echo "Set: ulimit -c unlimited"
+ulimit -n 500000
+echo "Set: ulimit -n 500000"
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
