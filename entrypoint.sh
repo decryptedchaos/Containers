ec9874ef9b8505b566a00bba82c052c2c7ab0f0d
Joshua Kerr <nixgod@gmail.com>
2018-06-14 19:58:42 -0500

    change ulimit syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,13 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-ulimit -n 100000
-ulimit -n
+# Set Ulimit
+ulimit -c unlimited
+echo "Set: ulimit -c unlimited"
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo "container~# ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
